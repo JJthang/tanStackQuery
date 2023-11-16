995ec4bbfaa93cf084a707e36b0503c93c454937
JJthang <thangpdph@gmail.com>
2023-11-16 13:50:15 +0700

    Complete Pagination

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
-const fuctionGetData = () => {
-  return axios
-    .get("https://65532f3b5449cfda0f2e4651.mockapi.io/Information")
-    .then(({ data }) => data);
+const fuctionGetData = async () => {
+  const url = new URL(
+    "https://65532f3b5449cfda0f2e4651.mockapi.io/Information"
+  );
+  url.searchParams.append("completed", false);
+  url.searchParams.append("page", 1);
+  url.searchParams.append("limit", 10);
+  console.log(url);
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (error) {}
 };
 
 const handDeleteAPI = (id) => {
@@ -19,10 +27,16 @@ const handPostAPI = (data) => {
   );
 };
 
-const handSearchAPI = (page, search) => {
-  return axios.get(
-    `https://65532f3b5449cfda0f2e4651.mockapi.io/Information/?page=${page}&limit=10&name=${search}`
+const handSearchAPI = (page, search, limit) => {
+  console.log(limit);
+  const url = new URL(
+    "https://65532f3b5449cfda0f2e4651.mockapi.io/Information"
   );
+  url.searchParams.append("name", search);
+  url.searchParams.append("completed", false);
+  url.searchParams.append("page", page);
+  url.searchParams.append("limit", limit.length);
+  return axios.get(`${url.href}`);
 };
 
 const handUpdateAPI = (id, data) => {
